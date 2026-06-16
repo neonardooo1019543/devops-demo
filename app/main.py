58d382ab5daa9025e8b30ece034e6d92e5977c61
neon <neon@test.com>
2026-06-16 18:10:59 +0800

    change main

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ app.include_router(login_router)
 @app.get("/")
 def root():
     # version 字串之後做 blue-green / canary、確認新版上線時會用到
-    return {"service": "hello-world", "version": VERSION}
+    return {"service": "hello-noway", "version": VERSION}
 
 
 @app.get("/health")
